e9b2e1d0edb9442d6f12637d5617a229e688f22f
yuki-konn <gakusei.saru@gmail.com>
2024-12-17 05:50:20 -0600

    created files to test nodeJS

---
+++ controllers/greetingTest.js
@@ -0,0 +1,11 @@
+import sayHiTo from "../modules/greeting.js";
+import toNoCase from "to-no-case";
+import camel from "to-camel-case";
+import space from "to-space-case";
+
+const name1 = toNoCase("Miyamoto Musashi");
+const name2 = camel("Miyamoto Musashi");
+const name3 = space("MiyamotoMusashi");
+sayHiTo(name1);
+sayHiTo(name2);
+sayHiTo(name3);
